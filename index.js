742e35e74fe9a3a5344515790d61510f63b36ecf
diabloevagto <diabloevagto@gmail.com>
2017-12-20 00:17:38 +0800

    add swiper component

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { AppRegistry } from 'react-native';
 // import App from './src/App';
-import App from './src/NavigatorComponent';
+// import App from './src/NavigatorComponent';
+import App from './src/basicSwiper';
 
 AppRegistry.registerComponent('reactNativeHello', () => App);
